ea423534c9e6cf3ff4ea3829a5f451633789162f
Jessica Prado <jessica.marie.prado@gmail.com>
2016-12-12 09:41:08 -0500

    movie-this works

--- liri.js
+++ liri.js
@@ -1,23 +1,39 @@
-var keys = require('keys.js');
-var Twitter = require('twitter');
+var twitter = require('twitter');
+var keys = require('./keys.js');
+var request = require("request");
 
-function my-tweets() {
-	var params = {screen_name: 'jmprado12'};
-	client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  	if (!error) {
-    console.log(tweets);
-  }
+
+//Twitter
+if (process.argv[2] == "my-tweets") {
+	var params = {
+			q: '@jmprado12',
+			count: 20
+		};
+
+var url = 'https://twitter.com/jmprado12?'
+var client = keys.twitterKeys;
+client.get(url, params, function(error, tweets, response) {
+ if (!error) {
+ console.log(tweets);
+ };
 });
-};
+}; //end of if/else statement
 
-function spotify-this-song() {
 
-};
 
-function movie-this(){
 
-};
 
-function do-what-it-says(){
 
+
+// Movies
+if (process.argv[2] == "movie-this") {
+
+	var movie = process.argv[3]
+	request("http://www.omdbapi.com/?t=" + movie + "&plot=short", function(error, response, body) {
+
+  // If the request is successful (i.e. if the response status code is 200)
+  	if (!error && response.statusCode === 200) {
+    console.log(response.body);
+  	}
+	});
 };
